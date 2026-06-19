c85db81e1d5248cc3dd77ef71da16aad4c3134df
VicooDi <jihedbenslimen54@gmail.com>
2026-06-19 11:47:05 +0100

    patrol

--- scripts/next_patrol_point.gd
+++ scripts/next_patrol_point.gd
@@ -1,54 +1,29 @@
 extends ActionLeaf
 
-@export var point_A:Vector3 = Vector3(0.0,2.0,0.0)
-@export var point_B:Vector3 = Vector3(2.0,2.0,0.0)
+@export var patrol_points:PackedVector3Array
+var curr_patrol_pt = 0
 
+@onready var navAgent: NavigationAgent3D = $"../../../../NavigationAgent3D"
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
-	if(blackboard.get_value("is_at_ptA")):
-		return go_pt_B(blackboard, actor)
-	else:
-		return go_pt_A(blackboard, actor)
-	
+	curr_patrol_pt = blackboard.get_value("crrPtrlPnt")
+	if(curr_patrol_pt == null):
+		curr_patrol_pt = 0
 
-func go_pt_A(blackboard, actor):
-	print("going to point A")
+	navAgent.set_target_position(patrol_points[curr_patrol_pt])
 	
-	var dir = (point_A - actor.global_position).normalized()
+	var dir = (navAgent.get_next_path_position() - actor.global_position).normalized()
+	
 	actor.velocity = dir * 80.0 * get_physics_process_delta_time()
 	actor.move_and_slide()
 	
-	var forward_dir = Vector3.FORWARD.rotated(Vector3.UP, -90)
-	#actor.rotation.y = lerp(actor.rotation.y, dir.angle_to(point_B), 0.1)
-	actor.rotation.y = forward_dir.angle_to(dir)
-	print(rad_to_deg(forward_dir.angle_to(dir)))
-	
-	if(vec_approx(actor.global_position, point_A)):
-		blackboard.set_value("is_at_ptA", true)
-		blackboard.set_value("is_at_ptB", false)
-		
+	if(vec_approx(actor.global_position, patrol_points[curr_patrol_pt])):
+		curr_patrol_pt+=1
+		blackboard.set_value("crrPtrlPnt", curr_patrol_pt)
+		print("Im here")
 		return SUCCESS
 	return RUNNING
-	
-func go_pt_B(blackboard, actor):
-	print("going to point B")
-	
-	var dir = (point_B - actor.global_position).normalized()
-	actor.velocity = dir * 80.0 * get_physics_process_delta_time()
-	actor.move_and_slide()
-	
-	var forward_dir = Vector3.FORWARD.rotated(Vector3.UP, -90)
-	#actor.rotation.y = lerp(actor.rotation.y, dir.angle_to(point_A), 0.1)
-	actor.rotation.y = forward_dir.angle_to(dir)
-	print(rad_to_deg(forward_dir.angle_to(dir)))
-	
-	if(vec_approx(actor.global_position, point_B)):
-		blackboard.set_value("is_at_ptB", true)
-		blackboard.set_value("is_at_ptA", false)
-		
-		return SUCCESS
-	return RUNNING
-
 
 func vec_approx(a: Vector3, b: Vector3) -> bool:
-	return abs((a - b).length()) < 0.05
+	print("Im not here")
+	return abs((a - b).length()) < 1
